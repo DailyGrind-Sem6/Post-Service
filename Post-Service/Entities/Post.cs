ab2b1189892f61a59d1479ab228ab36ea4bba93b
Gökay <gokayatalay2@outlook.com>
2024-04-09 14:43:07 +0200

    Edited entity

--- Post-Service/Entities/Post.cs
+++ Post-Service/Entities/Post.cs
@@ -1,11 +1,14 @@
 using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Post_Service.Entities;
 
 public class Post
 {
-    [Key]
-    public int Id { get; set; }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
     public DateTime CreatedAt { get; set; }
