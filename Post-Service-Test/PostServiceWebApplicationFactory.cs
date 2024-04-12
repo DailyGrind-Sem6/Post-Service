76c30f09cc5d482841beb3252f1fafae980dfd9b
Gökay <gokayatalay2@outlook.com>
2024-04-12 16:28:55 +0200

    In-memory database integration tests (Get and Post)

--- Post-Service-Test/PostServiceWebApplicationFactory.cs
+++ Post-Service-Test/PostServiceWebApplicationFactory.cs
@@ -2,16 +2,43 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Mongo2Go;
+using MongoDB.Driver;
+using Post_Service.Repositories;
 
 namespace Post_Service_Test;
 
 public class PostServiceWebApplicationFactory : WebApplicationFactory<Program>
 {
+    private MongoDbRunner _runner;
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
         {
-            
+            // Remove the existing registration for IPostRepository
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(IPostRepository));
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            // Start MongoDbRunner
+            _runner = MongoDbRunner.Start();
+
+            // Register the MongoDB client with the connection string provided by MongoDbRunner
+            services.AddSingleton<IMongoClient>(new MongoClient(_runner.ConnectionString));
+
+            // Register the repository with the MongoDB client
+            services.AddSingleton<IPostRepository, PostRepository>();
         });
+    }
+    
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        if (disposing)
+        {
+            _runner.Dispose();
+        }
     }
 }
